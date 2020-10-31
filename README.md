1d43c2673e79a39ac145d7e1ea169ab0cb52a4d5
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-10-31 11:37:49 -0300

    r7: Initial TWRP Device Tree commit
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

---
+++ README.md
@@ -0,0 +1,24 @@
+Device Tree (Recovery) for Samsung Galaxy Note 10 Lite (r7)
+===========================================
+
+The Samsung Galaxy Note 10 Lite (codenamed _"r7"_) is a high-end smartphone from Samsung.
+It was announced on January 2020.
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core (4x2.0 GHz Cortex-A55 & 4x2.0 GHz Cortex-A55)
+Chipset | Exynos 9810 (10 nm)
+GPU     | Mali-G72 MP18
+Memory  | 6/8 GB RAM
+Shipped Android Version | 9 (upgradable to Android 10)
+Storage | 128 GB
+Battery | LiPo 4500mAh battery
+Display | 1080 x 2400 pixels, 20:9 ratio (~394 ppi density)
+Rear Camera  | 12 MP, f/1.7, 27mm (wide), 1/2.55", 1.4µm, Dual Pixel PDAF, OIS
+Rear Camera  | 12 MP, f/2.4, 52mm (telephoto), 1/3.6", 1.0µm, PDAF, OIS, 2x optical zoom
+Rear Camera  | 12 MP, f/2.2, 12mm (ultrawide)
+Front Camera  | 32 MP, f/2.2, 25mm (wide), 1/2.8", 0.8µm
+
+Copyright 2020 - YukoSky/Velosh.
+
+![Samsung Galaxy Note 10 Lite](https://fdn2.gsmarena.com/vv/bigpic/sasmung-galaxy-note10-lite-r.jpg "Samsung Galaxy Note 10 Lite")
---
+++ omni_r7.mk
@@ -0,0 +1,46 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Copyright (C) 2020 Team Win Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from Note 10 Lite device
+$(call inherit-product, device/samsung/r7/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := r7
+PRODUCT_NAME := omni_r7
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-N770F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := samsung SM-N770F
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+    
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+# Others
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
